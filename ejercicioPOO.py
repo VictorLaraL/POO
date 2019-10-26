4e1fbf5c392238572e5cf812be5ce236227895fc
Victor Manuel Lara Lopez <vicblack15@hotmail.com>
2019-10-26 00:10:35 -0500

    Update ejercicioPOO.py

--- ejercicioPOO.py
+++ ejercicioPOO.py
@@ -1,7 +1,7 @@
 import math
 
 class Punto():
-    #Clase que genera los puntos en el plano
+    # Clase que genera los puntos en el plano
     def __init__(self, x, y):
         self.__x = x
         self.__y = y
@@ -28,30 +28,30 @@ class PoligonoR():
     def __init__(self, puntos):
         self.puntos = puntos
         
-
     def area(self):
+        # Calculamos el area del poligono dependiendo del numero de puntos que tenga
         if(len(self.puntos) == 3 and self.comprobacionPI()):
             triangulo = Triangulo(self.puntos)
-            return triangulo.area()
-                
+            return triangulo.area()       
         elif(len(self.puntos) == 4 and self.comprobacionPI()):
             cuadrado = Cuadrado(self.puntos)
             return cuadrado.area()
-
         elif(len(self.puntos) >= 5 and self.comprobacionPI()):
             return self.areaPolReg()
         else:
             print('No cuentas con los puntos necesarios!')
 
     def areaPolReg(self):
+        # Calculamos el area de poligonos regulares de 5 o mas lados
         cantPunt = len(self.puntos)
-    
-        if (cantPunt % 2 == 0):
+
+        # Calculo del area de un poligono regular de cantidad de puntos pares
+        if (cantPunt % 2 == 0): 
             apotema = (self.distDosPuntos(self.puntoMedio(self.puntos[0], self.puntos[1]), (self.puntoMedio(self.puntos[cantPunt/2], self.puntos[(cantPunt/2)+1]))))/2
             areaP = (self.perimetro() * apotema)/2
             return areaP    
         else:
-            # Calcular area del poligono de lados impares
+            # Calcular area del poligono regular de lados impares
             pass
 
     def perimetro(self):
@@ -61,25 +61,30 @@ class PoligonoR():
         return perimetro
 
     def comprobacionPI(self):
+        # Comprobamos que sea un Poligono y que sea regular 
         dist1 = self.distDosPuntos(self.puntos[0], self.puntos[1])
         dist2 = self.distDosPuntos(self.puntos[1], self.puntos[2])
-        for x in range(3,len(self.puntos)): 
-            if (dist1 != dist2 and dist2 ): # Comprobacionentre las caras de la figura y que el ultimo punto se conecte con el primero
+        for x in range(3,len(self.puntos)):
+            # Comprobacion entre las aristas de la figura y que el ultimo punto se conecte con el primero 
+            if (dist1 != dist2 or dist2 != self.distDosPuntos(self.puntos[0], self.puntos[len(self.puntos)])): 
                 return False
             dist1 = dist2
             dist2 = self.distDosPuntos(self.puntos[x-1], self.puntos[x])            
         return True
 
     def distDosPuntos(self, p1, p2):
+        # Calculamos la distancia entre dos puntos
         distancia = math.sqrt(((p2.getX() - p1.getX())**2) + ((p2.getY() - p1.getY())**2))
         return distancia
 
     def puntoMedio(self, p1, p2):
+        # Calculamos el punto medio entre dos puntos
         x = (p1.getX() - p2.getX())/2
         y = (p1.getY() - p2.getY())/2
         return Punto(x,y)
 
     def imprimirPuntos(self):
+        # Impresion de los puntos en un objeto Poligono
         print('los puntos del poligono son: ')
         for punto in self.puntos:
             print(punto)
@@ -89,6 +94,7 @@ class PoligonoR():
 
 
 class Triangulo(PoligonoR):
+    # Clase que hereda de PoligonoR y calcula el area de un triangulo regular
     def __init__(self, puntos):
         super().__init__(puntos)
 
@@ -99,6 +105,7 @@ class Triangulo(PoligonoR):
         return areaT 
         
 class Cuadrado(PoligonoR):
+    # Clase que hereda de PoligonoR y calcula el area de un cuadrado (poligono reg de 4 lados)
     def __init__(self, puntos):
         super().__init__(puntos)
 
